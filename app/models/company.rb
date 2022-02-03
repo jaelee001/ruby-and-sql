fc5e6328a400e96c869c93f0555ef64a1e278b84
jaelee001 <jaehee.lee@kellogg.northwestern.edu>
2022-02-03 02:37:08 +0000

    code_along_2

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,10 @@
 class Company < ApplicationRecord
+    has_many:contacts 
+    #same thing as below
+    
+    # def contacts
+    #     Contact.where({company_id: id})
+    # end
+
+
 end
--- code-along/2-associations.rb
+++ code-along/2-associations.rb
@@ -4,6 +4,21 @@
 # rails runner code-along/2-associations.rb
 
 # 1. Query all of the contacts at Apple Inc.
+apple = Company.where({name: "Apple"})[0]
+
+# contacts = Contact.where({company_id: apple.id}) #this has now been put into company.rb
+# puts contacts.inspect
+
+# apple.contacts
+contacts = apple.contacts
+
+# for contact in apple.contacts
+#     puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+# end
+
+for contact in contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+end
 
 # 2. add association to contacts in Company model
 
